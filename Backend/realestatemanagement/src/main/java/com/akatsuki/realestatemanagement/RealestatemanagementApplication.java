2e4324bbf5ee5ce57cfaba489c97b90bf7e163a2
Bahae9 <lemtaibahae9@gmail.com>
2024-05-04 00:14:19 +0100

    setting up spring-boot

---
+++ Backend/realestatemanagement/src/main/java/com/akatsuki/realestatemanagement/RealestatemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.akatsuki.realestatemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestatemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestatemanagementApplication.class, args);
+	}
+
+}
---
+++ Backend/realestatemanagement/src/test/java/com/akatsuki/realestatemanagement/RealestatemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akatsuki.realestatemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestatemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
